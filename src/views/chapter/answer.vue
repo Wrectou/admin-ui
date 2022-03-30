2ce8430cb53e88986937326d3f72775b152cfd30
wrectou <wrectou@gmail.com>
2022-03-30 17:33:11 +0800

    单选做一半

--- src/views/chapter/answer.vue
+++ src/views/chapter/answer.vue
@@ -6,7 +6,7 @@
         <div class="type">单项选择题</div>
         <div class="chapter">当前章节：社会主义法制理念</div>
       </div>
-      <div class="number"><span>1</span>/50</div>
+      <div class="number" @click="isShowDrawer=true"><span>1</span>/50</div>
     </div>
 
     <el-divider />
@@ -16,15 +16,109 @@
       <p class="question-title">
         <span class="fraction">({{questionObj.fraction}}分)</span>
         <div class="title">{{questionObj.question}}</div>
+        <div class="collect">
+          <el-icon><star-filled /></el-icon>
+          <el-icon><star /></el-icon>
+        </div>
       </p>
       
       <div class="answer-box">
+
+        <!-- 选项组 -->
         <el-radio-group v-model="answerObj.answer">
-          <el-radio v-for="item in questionObj.answerList" :key="item.value" :value="item.label" :label="item.value" size="large">{{item.label}}</el-radio>
+
+          <!-- <el-radio v-for="item in questionObj.answerList" :key="item.value" :value="item.label" :label="item.value" size="large">{{item.label}}</el-radio> -->
+
+          <!-- 未选中项目 -->
+          <label class="el-radio">
+            <span class="el-radio__input">
+              <span class="el-radio__inner">A</span>
+              <input class="el-radio__original" type="radio">
+            </span>
+            <span class="el-radio__label">依法治国</span>
+          </label>
+
+          <!-- 多选正确项但是没选 -->
+          <label class="el-radio">
+            <span class="el-radio__input is-checked primary">
+              <span class="el-radio__inner">B</span>
+              <input class="el-radio__original" type="radio">
+            </span>
+            <span class="el-radio__label">依法治国</span>
+          </label>
+
+          <!-- 正确 -->
+          <label class="el-radio">
+            <span class="el-radio__input is-checked">
+              <span class="el-radio__inner">
+                <el-icon><check /></el-icon>
+              </span>
+              <input class="el-radio__original" type="radio">
+            </span>
+            <span class="el-radio__label">依法治国</span>
+          </label>
+
+          <!-- 错误 -->
+          <label class="el-radio">
+            <span class="el-radio__input is-checked danger">
+              <span class="el-radio__inner">
+                <el-icon><close /></el-icon>
+              </span>
+              <input class="el-radio__original" type="radio">
+            </span>
+            <span class="el-radio__label">依法治国</span>
+          </label>
+
         </el-radio-group>
+
+        <div class="answer-bar">
+          <div class="default-answer">答案：A</div>
+          <div class="user-answer">你的选择：B，用时： 5秒</div>
+        </div>
+
       </div>
 
+
+      <div class="tit">统计</div>
+      <div class="statistics-box">
+        <div class="item">
+          <span>全站作答</span>
+          <div class="num">300次</div>
+        </div>
+        <div class="item">
+          <span>全站正确率</span>
+          <div class="num">90%</div>
+        </div>
+        <div class="item">
+          <span>易错项</span>
+          <div class="num">B</div>
+        </div>
+      </div>
+
+
+      <div class="tit">解析</div>
+      <div class="analysis-box">
+        略的卢卡申科大家顺利到家啊啥的就撒的空间啊啥的了；就撒了；到家啊时间到了撒
+        略的卢卡申科大家顺利到家啊啥的就撒的空间啊啥的了；就撒了；到家啊时间到了撒
+        略的卢卡申科大家顺利到家啊啥的就撒的空间啊啥的了；就撒了；到家啊时间到了撒
+        略的卢卡申科大家顺利到家啊啥的就撒的空间啊啥的了；就撒了；到家啊时间到了撒
+        略的卢卡申科大家顺利到家啊啥的就撒的空间啊啥的了；就撒了；到家啊时间到了撒
+        略的卢卡申科大家顺利到家啊啥的就撒的空间啊啥的了；就撒了；到家啊时间到了撒
+        略的卢卡申科大家顺利到家啊啥的就撒的空间啊啥的了；就撒了；到家啊时间到了撒
+        略的卢卡申科大家顺利到家啊啥的就撒的空间啊啥的了；就撒了；到家啊时间到了撒
+        略的卢卡申科大家顺利到家啊啥的就撒的空间啊啥的了；就撒了；到家啊时间到了撒
+      </div>
+
+
     </div>
+
+    <el-drawer
+      v-model="isShowDrawer"
+      title="答题卡"
+      :direction="rtl"
+    >
+      <span @click="isShowDrawer=false">1 2 3 4 5 6</span>
+    </el-drawer>
 
   </div>
 </template>
@@ -49,6 +143,8 @@ const questionObj = reactive({
     {value: 4, label: '公平正义'},
   ]
 })
+
+let isShowDrawer = ref(false)
 
 
 </script>
@@ -78,8 +174,9 @@ const questionObj = reactive({
 }
 
 .content-box{
-  padding: 0 40px;
+  padding: 0 30px;
 }
+
 .question-title{
   display: flex;
   align-items: center;
@@ -88,23 +185,132 @@ const questionObj = reactive({
     color: rgb(64, 158, 255);
   }
   .title{
-    font-size: 18px;
+    flex: 1;
+    font-size: 20px;
   }
 }
+
+.answer-box{
+
+}
+
 ::v-deep(.el-radio-group){
   display: flex;
+  margin: 20px 0 0;
   .el-radio{
     flex: 0 0 100vw;
+    margin: 6px 0;
+    .el-radio__label{
+      font-size: 18px;
+      font-weight: 400;
+    }
+    .el-radio__inner{
+      width: 24px;
+      height: 24px;
+      color: #999;
+      text-align: center;
+      line-height: 22px;
+      font-weight: 400;
+    }
+    .el-radio__input{
+      margin: 2px 0 0 0;
+    }
   }
-  .el-radio.el-radio--large .el-radio__label{
+  .el-radio__input.is-checked .el-radio__inner {
+    border-color: #67c23a;
+    background: #67c23a;
+  }
+  .el-radio__input.is-checked+.el-radio__label{
+    color: #67c23a;
+  }
+  .el-radio__input.primary .el-radio__inner {
+    border-color: #409eff;
+    background: #409eff;
+    color: #fff;
+  }
+  .el-radio__input.primary+.el-radio__label{
+    color: #409eff;
+  }
+  .el-radio__input.danger .el-radio__inner {
+    border-color: #f56c6c;
+    background: #f56c6c;
+  }
+  .el-radio__input.danger+.el-radio__label{
+    color: #f56c6c;
+  }
+  .el-radio__input.is-checked .el-radio__inner::after{
+    transform: scale(0);
+  }
+  .el-icon{
+    margin: 4px 0 0;
+    color: #fff;
+  }
+}
+
+.answer-bar{
+  display: flex;
+  align-items: end;
+  margin: 20px 0 0;
+  background: #f3f3f3;
+  padding: 14px 10px;
+  font-size: 14px;
+  .default-answer{
+    margin: 0 20px 0 0;
+    color: #409eff;
     font-size: 16px;
   }
-  .el-radio.el-radio--large .el-radio__inner{
-    width: 16px;
-    height: 16px;
+}
+
+.tit{
+  position: relative;
+  margin: 26px 0 10px;
+  padding: 0 0 0 14px;
+  font-size: 20px;
+}
+.tit:before{
+  content: '';
+  position: absolute;
+  top: 4px;
+  left: 0;
+  width: 4px;
+  height: 20px;
+  background: rgb(64, 158, 255);
+}
+
+.statistics-box{
+  display: flex;
+  align-items: center;
+  background: #f3f3f3;
+  padding: 8px 10px;
+  font-size: 14px;
+  .item{
+    flex: 0 0 200px;
+    text-align: center;
+    >span{
+      color: #666;
+    }
   }
-  .el-radio__input{
-    margin: 2px 0 0 0;
+  .item:nth-child(1) {
+    .num{
+      color: #333;
+    }
   }
+  .item:nth-child(2) {
+    border-left: solid 1px #e3e3e3;
+    .num{
+      color: #67c23a;
+    }
+  }
+  .item:nth-child(3) {
+    border-left: solid 1px #e3e3e3;
+    .num{
+      color: #f56c6c;
+    }
+  }
+}
+
+.analysis-box{
+  padding: 10px;
+  background: #f3f3f3;
 }
 </style>
